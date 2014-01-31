1c4d56bbe15f17a872a216fcc53e1a63683d1c5c
Samuel A. Yallop <syallop@gmail.com>
2014-01-31 23:20:35 +0000

    Adds synonym for nicer tuple-nesting.

--- Join/Language/Types.hs
+++ Join/Language/Types.hs
@@ -1,11 +1,18 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
 module Join.Language.Types
     (Channel(Channel)
     ,Channels()
     ,(&)
     ,(&|)
     ,single
+    ,(:&)
     )where
+
+-- | Type synonym for right-fixity tuples.
+-- E.G. Int :& Char :& Bool = (Int,(Char,Bool))
+type a :& b = (a,b)
+infixr 7 :&
 
 -- | A typed Channel, uniquely identified by an Int
 data Channel a = Channel Int
@@ -16,20 +23,20 @@ instance Show (Channel a) where
 -- | A combination of one or many Channel's
 data Channels a where
     SingleChannel :: Channel a -> Channels a
-    ConsChannel   :: Channel a -> Channels b -> Channels (a,b)
+    ConsChannel   :: Channel a -> Channels b -> Channels (a :& b)
 
 instance Show (Channels a) where
     show (SingleChannel c)  = show c
     show (ConsChannel c cs) = show c ++ "&" ++ show cs
 
 -- | Infix, cons a Channel to Channels
-(&) :: Channel a -> Channels b -> Channels (a,b)
+(&) :: Channel a -> Channels b -> Channels (a :& b)
 c & cs = c `ConsChannel` cs
 
 infixr 7 &
 
 -- | Infix, cons a Channel to a final Channel.
-(&|) :: Channel a -> Channel b -> Channels (a,b)
+(&|) :: Channel a -> Channel b -> Channels (a :& b)
 c &| d = c `ConsChannel` SingleChannel d
 
 -- | Prefix, 'promote' a single Channel to Channels
