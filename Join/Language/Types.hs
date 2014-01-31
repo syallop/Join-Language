5b1b445ed8282e7ded6f001497ef0cbdc07a0b43
Samuel A. Yallop <syallop@gmail.com>
2014-01-31 22:48:02 +0000

    Adds 'show' instances for Channel/s

--- Join/Language/Types.hs
+++ Join/Language/Types.hs
@@ -4,10 +4,17 @@ module Join.Language.Types where
 -- | A typed Channel, uniquely identified by an Int
 data Channel a = Channel Int
 
+instance Show (Channel a) where
+    show (Channel i) = "Channel-" ++ show i
+
 -- | A combination of one or many Channel's
 data Channels a where
     SingleChannel :: Channel a -> Channels a
     ConsChannel   :: Channel a -> Channels b -> Channels (a,b)
+
+instance Show (Channels a) where
+    show (SingleChannel c)  = show c
+    show (ConsChannel c cs) = show c ++ "&" ++ show cs
 
 -- | Infix, cons a Channel to Channels
 (&) :: Channel a -> Channels b -> Channels (a,b)
