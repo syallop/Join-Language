2da9f1ecce769c174debc985787ed3401465597f
Samuel A. Yallop <syallop@gmail.com>
2014-02-01 00:34:07 +0000

    Adds description Interpreter.
    - Adds a 'describe' Interpreter of ProcessM which describes each
      instruction on stdout.

---
+++ Join/Language/Interpretation/Describe.hs
@@ -0,0 +1,21 @@
+module Join.Language.Interpretation.Describe where
+
+import Join.Language
+import Join.Language.Types
+
+import Control.Monad (void)
+
+-- | Interpret a ProcessM by describing each instruction on stdout.
+describe :: Interpretation IO
+describe = Interpretation
+    { iDef        = \c _ -> void $ putStrLn $ "Define a pattern for " ++ show c
+    , iInert      = putStrLn "End."
+    , iNewChannel = putStrLn "Request new Channel" >> return (Channel 0)
+    , iSpawn      = \c _ -> void $ putStrLn $ "Spawn a value on " ++ show c
+    , iWith       = \ p q -> do putStrLn "Simultaneously run: ( "
+                                interpret describe p
+                                putStr ", "
+                                interpret describe q
+                                putStrLn ")"
+    }
+
