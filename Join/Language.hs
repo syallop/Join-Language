eb667d5b1b05c9c942c16a5d52c6cda61f1762a3
Samuel A. Yallop <syallop@gmail.com>
2014-02-02 19:12:20 +0000

    Changes 'def' patterns to pass arguments naturally rather than tupled.
    - Replaces 'Channels' in 'Types.hs' with 'ChannelPattern' in
      'Language.hs' which builds nested (->) instead of (,).
    
    - Updates Def definition such that the RHS has type of form:
      :: a -> b ... -> n -> m -> ProcessM ()
      Instead of:
      :: (a,(b,...(n,m))) -> ProcessM ()
      Allowing more natural Haskell functions to be written.

--- Join/Language.hs
+++ Join/Language.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
 module Join.Language
     ( Instruction(..)
     , ProcessM
@@ -11,12 +12,18 @@ module Join.Language
 
     , Interpretation(..)
     , interpret
+
+    , ChannelPattern(..)
+    , on
+    , (&)
+    , (&|)
     ) where
 
 import Join.Language.Types
 
 import Control.Monad.IO.Class
 import Control.Monad.Operational (ProgramT,ProgramViewT(..),singleton,viewT)
+
 
 -- | Single primitive instructions in a join process.
 -- Constructors vaguely commented with intended semantics - how the
@@ -25,8 +32,8 @@ import Control.Monad.Operational (ProgramT,ProgramViewT(..),singleton,viewT)
 data Instruction a where
 
     -- | Join definition.
-    Def        :: Channels a              -- ^ Channels matched for.
-               -> (a -> ProcessM ())      -- ^ Handler function called.
+    Def        :: ChannelPattern p        -- ^ Channels matched for.
+               -> p                       -- ^ Handler function called.
                -> Instruction ()          -- ^ No result, asynchronous.
 
     -- | End process.
@@ -45,12 +52,45 @@ data Instruction a where
                -> ProcessM ()             -- ^ Second process.
                -> Instruction ()          -- ^ No result, asynchronous execution of both Processes.
 
+
+
+-- | A patten of one or many Channel's. Type variable denotes the type of
+-- a function which accepts the type of each conjunctive Channel in order
+-- and produces a ProcessM (). This type corresponds to the type of the
+-- function in the RHS of a join definition.
+--
+-- For convenience, a ChannelPattern may be built using 'on' and infix functions '&'
+-- '&|'.
+-- E.g. Matching on (a, b, c, ... ,n ,m) == (a & b & c &...n &| m)
+data ChannelPattern p where
+    LastChannel :: Channel a -> ChannelPattern (a -> ProcessM ())
+    AndChannels :: Channel a -> ChannelPattern p -> ChannelPattern (a -> p)
+
+instance Show (ChannelPattern p) where
+    show (LastChannel c   ) = show c
+    show (AndChannels c cs) = show c ++ " & " ++ show cs
+
+-- | Infix, cons a Channel to a ChannelPattern.
+(&) :: Channel a -> ChannelPattern p -> ChannelPattern (a -> p)
+c & cs = c `AndChannels` cs
+infixr 7 &
+
+-- | Infix, cons a Channel to a final Channel of a ChannelPattern.
+(&|) :: Channel a -> Channel b -> ChannelPattern (a -> b -> ProcessM ())
+c &| d = c & LastChannel d
+
+-- | 'Promote' a single Channel to a ChannelPattern.
+on :: Channel a -> ChannelPattern (a -> ProcessM ())
+on = LastChannel
+
+
+
 -- | A single join Process. Implemented monadically over Instruction with the
 -- Operational Monad.
 type ProcessM a = ProgramT Instruction IO a
 
 -- | Enter a single Def Instruction into ProcessM.
-def :: forall a. Channels a -> (a -> ProcessM ()) -> ProcessM ()
+def :: forall p. ChannelPattern p -> p -> ProcessM ()
 def c p = singleton $ Def c p
 
 -- | Enter a single Inert Instruction into ProcessM.
@@ -85,13 +125,13 @@ interpret i m = do inst <- liftIO $ viewT m
 
                         With       p q :>>= k -> iWith i p q >> interpret i (k ())
 
-
 -- | An Interpretation is a collection of functions used to interpret
 -- a ProcessM computation.
 data Interpretation m = Interpretation
-    { iDef        :: forall a. Channels a -> (a -> ProcessM ()) -> m ()
+    { iDef        :: forall p. ChannelPattern p -> p -> m ()
     , iInert      :: m ()
     , iNewChannel :: forall a. m (Channel a)
     , iSpawn      :: forall a. Channel a -> a -> m ()
     , iWith       :: ProcessM () -> ProcessM () -> m ()
     }
+
