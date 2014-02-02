5eeb22391b5a6408b3376c6c6d31e9756fefeeee
Samuel A. Yallop <syallop@gmail.com>
2014-02-02 19:53:46 +0000

    Adds Simple language example.

---
+++ Join/Language/Examples.hs
@@ -0,0 +1,28 @@
+module Join.Language.Examples where
+
+import Join.Language
+
+import Control.Monad.IO.Class
+
+countDown :: Int -> ProcessM ()
+countDown n = do
+    -- Get a new Channel, inferred to be of type 'Channel Int'.
+    intChannel <- newChannel
+
+    -- Make a join definition:
+    --  When there's an int on intChannel: 
+    --    print it. If 0, do nothing.
+    --    Otherwise spawn a de-incremented int on intChannel.
+    def (on intChannel) (\i -> do liftIO $ print i
+                                  if i == 0
+                                    then inert
+                                    else spawn intChannel (i-1))
+
+    -- Spawn the starting number on intChannel.
+    spawn intChannel n
+
+    -- Become inert.
+    inert
+
+    -- Instructions below ignored <= previous 'inert' instruction.
+    spawn intChannel 1000
