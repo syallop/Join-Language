c47c94d5ffb2f0ef95f84068629f2a28aac75302
Samuel A. Yallop <syallop@gmail.com>
2014-02-27 00:43:27 +0000

    Adds simple/ inefficient interpreter.
    - Adds Join.Interpreter.Simple providing an example interpreter for Join
      Language programs, implemented quickly and inefficiently for example
      and proof of feasibility of the Language.

---
+++ Join/Interpretation/Simple.hs
@@ -0,0 +1,211 @@
+{-# LANGUAGE GADTs #-}
+module Join.Interpretation.Simple where
+
+import Prelude hiding (lookup)
+
+import Join.Interpretation.Simple.Types
+import Join.Language
+import Join.Language.Types
+
+import Control.Concurrent.MVar
+import Control.Concurrent
+import Control.Monad
+import Control.Monad.Operational
+import Control.Monad.IO.Class
+import Data.Map hiding (map)
+import Data.Serialize (Serialize)
+
+import Join.Language.Examples
+
+-- | State of the entire interpretation.
+data IState = IState
+    { msgQueue   :: MsgQueue
+    , matchRules :: MatchRules
+    }
+
+-- | Reference to state of interpretation.
+data IStateRef = IStateRef (MVar IState)
+
+-- | Get a reference to the state of a new interpretation.
+newIStateRef :: IO IStateRef
+newIStateRef = liftM IStateRef (newMVar $ IState mkMsgQueue [])
+
+-- | From a reference to the interpretation state, take the current state.
+takeIStateRef :: IStateRef -> IO IState
+takeIStateRef (IStateRef r) = takeMVar r
+
+-- | Into an reference to the interpretation state, put a new state.
+putIStateRef ::IStateRef -> IState -> IO ()
+putIStateRef (IStateRef r) = putMVar r
+
+-- | State of interpretation of a single ProcessM.
+data PState = PState
+    { iStateRef     :: IStateRef
+    , replyChannels :: Map ChanId ChanId
+    }
+
+-- | Create a new top-level Process state (new IState).
+newPState :: IO PState
+newPState = do
+    is <- newIStateRef
+    return $ PState is empty
+
+-- | Create a Process execution state, given an existing interpretation
+-- state.
+mkPState :: IStateRef -> PState
+mkPState iStR = PState iStR empty
+
+-- | Run a top-level ProcessM.
+run :: ProcessM a -> IO a
+run p = do
+    st    <- newPState
+    forkIO $ ruleHandler (iStateRef st)
+    runWith st p
+
+-- | With state to be ran under, run a ProcessM to produce an IO action.
+runWith :: PState -> ProcessM a -> IO a
+runWith st@(PState iStR rcs) p = do
+    instr <- liftIO $ viewT p
+    case instr of
+
+        -- ^ Operational return.
+        Return a     -> return a
+
+        -- ^ Def pattern.
+        Def dp p
+            :>>= k -> do iSt <- takeIStateRef iStR
+                         let rs   = matchRules iSt
+                             rs'  = registerMatchRule (mkChanMatches dp) p rs
+                             iSt' = iSt{matchRules = rs'}
+                         putIStateRef iStR iSt'
+                         runWith st (k ())
+
+        -- ^ Request a new Channel.
+        NewChannel
+            :>>= k -> do iSt <- takeIStateRef iStR
+                         cId <- newChanId
+                         let c     = Channel (getChanId cId)
+                             msgQ  = msgQueue iSt
+                             msgQ' = registerChanId cId msgQ
+                             iSt'  = iSt{msgQueue = msgQ'}
+                         putIStateRef iStR iSt'
+                         runWith st (k c)
+
+        -- ^ Send a message to a Channel.
+        Send c m
+            :>>= k -> do iSt <- takeIStateRef iStR
+                         let msgQ  = msgQueue iSt
+                             msgQ' = registerMsg c (mkMsg (mkMsgData m) Nothing) msgQ
+                             iSt'  = iSt{msgQueue = msgQ'}
+                         putIStateRef iStR iSt'
+                         runWith st (k ())
+
+        -- ^ Spawn a concurrent execution of a ProcessM.
+        Spawn p
+            :>>= k -> do forkIO $ runWith st p
+                         runWith st (k ())
+
+        -- ^ Synchronously spawn a value on a Channel, and wait for
+        -- a result.
+        Sync s@(SyncChannel c) m
+            :>>= k -> do iSt <- takeIStateRef iStR
+                         rId <- newChanId
+                         let r     = Channel (getChanId rId)
+                             msgQ  = msgQueue iSt
+                             msgQ' = registerMsg c (mkMsg (mkMsgData m) (Just rId)) msgQ
+                             iSt'  = iSt{msgQueue = msgQ'}
+                         putIStateRef iStR iSt'
+                         v <- new
+                         forkIO $ waitOn iStR r v
+                         runWith st (k v)
+
+        -- ^ Spawn a value on the reply Channel of a SyncChannel.
+        Reply (SyncChannel c) m
+            :>>= k -> do iSt <- takeIStateRef iStR
+                         let rs   = replyChannels st
+                             i    = mkChanId c
+                             mj   = lookup i rs
+                             msgQ = msgQueue iSt
+                         case mj of
+                             Nothing -> error "No reply Channel."
+                             Just j  -> let msgQ' = registerMsg (Channel (getChanId j)) (mkMsg (mkMsgData m) Nothing) msgQ
+                                            iSt'  = iSt{msgQueue = msgQ'}
+                                           in putIStateRef iStR iSt'
+                                           >> runWith st (k ())
+
+        With p q
+            :>>= k -> do forkIO $ runWith st p
+                         forkIO $ runWith st q
+                         runWith st (k ())
+
+-- | Responsible for triggering matching rule definitions.
+ruleHandler :: IStateRef -> IO ()
+ruleHandler iStR = forever $ do
+    iSt <- takeIStateRef iStR
+    let rs   = matchRules iSt
+        msgQ = msgQueue iSt
+    msgQ' <- tryAllRules rs msgQ
+    let iSt' = iSt{msgQueue = msgQ'}
+    putIStateRef iStR iSt'
+    yield
+    threadDelay 100000
+  where
+    -- | Try and execute all rules against a MsgQueue.
+    tryAllRules :: MatchRules -> MsgQueue -> IO MsgQueue
+    tryAllRules []     msgQ = return msgQ
+    tryAllRules (r:rs) msgQ = case tryMatchRule r msgQ of
+        Nothing                    -- Rule failed
+            -> tryAllRules rs msgQ -- Try the next..
+
+        Just (msgQ', msgs)                 -- Rule succeeded
+            -> do forkIO $ execRule r msgs -- Concurrently trigger the rule
+                  tryAllRules rs msgQ'     -- Try the next..
+
+    -- | Execute a matched MatchRule on the corresponding Messages.
+    execRule :: MatchRule -> Msgs -> IO ()
+    execRule (MatchRule cms f) msgs =
+        let rs    = extractReplyIds cms msgs
+            iStR' = (mkPState iStR){replyChannels = fromList rs}
+            bs    = map (unMsgData . fst . unMsg) msgs
+            p     = apply f bs
+           in runWith iStR' p
+
+{-waitOn :: Serialize a => IStateRef -> Channel a -> IO a-}
+{-waitOn iStR c = do-}
+    {-iSt <- takeIStateRef iStR-}
+
+    {-let msgQ     = msgQueue iSt-}
+        {-matchAll = tryChanMatch (mkChanMatch $ All c) msgQ-}
+
+    {-case matchAll of-}
+        {-Nothing-}
+          {--> do putIStateRef iStR iSt-}
+                {-yield-}
+                {-threadDelay 100000-}
+                {-waitOn iStR c-}
+
+        {-Just (msgQ',m)-}
+          {--> do putIStateRef iStR (iSt{msgQueue = msgQ'})-}
+                {-let (d,_) = unMsg m-}
+                {-return $ unsafeDecodeMsgData d-}
+
+-- | Wait for any message on a given channel, and write it to a SyncVal.
+waitOn :: Serialize a => IStateRef -> Channel a -> SyncVal a -> IO ()
+waitOn iStR c r = do
+    iSt <- takeIStateRef iStR
+
+    let msgQ     = msgQueue iSt
+        matchAll = tryChanMatch (mkChanMatch $ All c) msgQ
+
+    case matchAll of
+        Nothing
+          -> do putIStateRef iStR iSt
+                yield
+                threadDelay 100000
+                waitOn iStR c r
+
+        Just (msgQ',m)
+          -> do putIStateRef iStR (iSt{msgQueue = msgQ'})
+                let (d,_) = unMsg m
+                write r (unsafeDecodeMsgData d)
+
---
+++ Join/Interpretation/Simple/Types.hs
@@ -0,0 +1,194 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs #-}
+module Join.Interpretation.Simple.Types
+    (-- Encode Message/ Channel types.
+     ChanId()
+    ,ReplyId
+    ,MsgData()
+    ,Msg()
+    ,Msgs
+    ,MsgQueue()
+    -- Smart constructors of Message/ Channel types.
+    , newChanId
+    , mkChanId
+    , getChanId
+    , mkMsgData
+    , unMsgData
+    , unsafeDecodeMsgData
+    , mkMsg
+    , unMsg
+    , mkMsgQueue
+
+    -- Encode Def Pattern types.
+    ,ChanMatch()
+    ,ChanMatches()
+    ,MatchRule(..)
+    ,MatchRules
+    , mkChanMatch
+    , mkChanMatches
+    , mkMatchRule
+
+    ,registerChanId
+    ,registerMsg
+    ,registerMatchRule
+
+    ,tryMsgMatch
+    ,tryChanMatch
+    ,tryChanMatches
+    ,tryMatchRule
+
+    ,extractReplyIds
+    ) where
+
+import Prelude hiding (lookup)
+
+import Join.Language
+import Join.Language.Types
+
+import Control.Arrow       (first)
+import Control.Applicative ((<$>))
+import Data.ByteString     (ByteString)
+import Data.Map            (Map,empty,insert,insertWith,lookup)
+import Data.Serialize      (decode,encode,Serialize)
+import Data.Unique         (newUnique,hashUnique)
+
+{-= Encode Message/ Channel types =-}
+-- | Unique identifier of a Channel.
+newtype ChanId = ChanId Int deriving (Eq, Ord)
+
+-- | Potential identifier on which a Reply should be sent.
+type ReplyId = Maybe ChanId
+
+-- | Encoding of a value send on a Channel.
+newtype MsgData = MsgData {unMsgData :: ByteString} deriving (Eq,Ord)
+
+-- | A single encoded value as sent on a Channel, tagged with a possible
+-- return ChanId.
+newtype Msg = Msg {unMsg :: (MsgData, ReplyId)}
+
+-- | Many Msg's.
+type Msgs = [Msg]
+
+-- | Map ChannelId's to all the Msg's waiting on them.
+newtype MsgQueue = MsgQueue {unMsgQueue :: Map ChanId Msgs}
+
+-- | Get a new, ChanId, guaranteed to be unique.
+newChanId :: IO ChanId
+newChanId = ChanId <$> hashUnique <$> newUnique
+
+mkChanId :: Channel a -> ChanId
+mkChanId (Channel i) = ChanId i
+
+-- | Get an Int representation of a ChanId.
+getChanId :: ChanId -> Int
+getChanId (ChanId i) = i
+
+mkMsgData :: Serialize a => a -> MsgData
+mkMsgData = MsgData <$> encode
+
+unsafeDecodeMsgData :: Serialize a => MsgData -> a
+unsafeDecodeMsgData (MsgData s) = case decode s of
+    Left  _ -> error "Invalid decode."
+    Right a -> a
+
+mkMsg :: MsgData -> ReplyId -> Msg
+mkMsg d i = Msg (d,i)
+
+mkMsgQueue :: MsgQueue
+mkMsgQueue = MsgQueue empty
+
+
+
+{-= Encode Def Pattern types =-}
+-- | Encodes a single item on the LHS of a Def Pattern.
+-- Match the first Msg sent to a ChanId/ first Msg equal to MsgData.
+newtype ChanMatch = ChanMatch (ChanId, Maybe MsgData)
+
+-- | Encodes the LHS of a Def Pattern.
+-- Match when all individual ChanMatch's simultaneously match.
+type ChanMatches = [ChanMatch]
+
+-- | Encodes an entire Def Pattern.
+-- When the ChanMatches match, a function should be triggered.
+data MatchRule = forall f. Apply f => MatchRule ChanMatches f
+
+-- Many Def Pattern's.
+type MatchRules = [MatchRule]
+
+-- | From a Language ChannelPattern, build an internal ChanMatch.
+mkChanMatch :: ChannelLike c a => ChannelPattern c a -> ChanMatch
+mkChanMatch (All  c  ) = let (Channel i) = getChannel c in ChanMatch (ChanId i, Nothing)
+mkChanMatch (Only c a) = let (Channel i) = getChannel c in ChanMatch (ChanId i, Just $ mkMsgData a)
+
+-- | From a Language DefPattern, build an internal ChanMatches.
+mkChanMatches :: DefPattern p -> ChanMatches
+mkChanMatches (LastPattern cp   ) = [mkChanMatch cp]
+mkChanMatches (AndPattern  cp dp) = mkChanMatch cp : mkChanMatches dp
+
+-- | From the LHS and RHS of a Language Def, build an internal MatchRule.
+mkMatchRule :: Apply f => DefPattern f -> f -> MatchRule
+mkMatchRule dp = MatchRule (mkChanMatches dp)
+
+
+
+
+
+{-= Primitive functions =-}
+-- | Register a new ChanId (With no waiting Msg's) in a MsgQueue.
+registerChanId :: ChanId -> MsgQueue -> MsgQueue
+registerChanId i = MsgQueue . insert i [] . unMsgQueue
+
+-- | Register, on a ChanId, a new Msg in a MsgQueue.
+registerMsg :: Channel a -> Msg -> MsgQueue -> MsgQueue
+registerMsg (Channel i) m = MsgQueue . insertWith (++) (ChanId i) [m] . unMsgQueue
+
+
+-- | Given ChanMatches and a corresponding trigger function, register it in
+-- a MatchRules collection.
+registerMatchRule :: Apply f => ChanMatches -> f -> MatchRules -> MatchRules
+registerMatchRule cms f rs = MatchRule cms f : rs
+
+-- | With a possible MsgData to require equality with, extract the first
+-- matching Msg from some Msgs.
+tryMsgMatch :: Maybe MsgData -> Msgs -> Maybe (Msgs,Msg)
+tryMsgMatch _         []    = Nothing
+tryMsgMatch Nothing  (m:ms) = Just (ms,m)
+tryMsgMatch (Just s) (Msg (s', mid) : ms)
+    -- String-match => Extract&return
+    | s == s'   = Just (ms,Msg (s', mid))
+
+    -- Recurse on remaining messages, re-prepending the non-matching message
+    -- to the result when Just.
+    | otherwise = first ((:) (Msg (s', mid))) <$> tryMsgMatch (Just s) ms
+
+-- | Given a ChanMatch, attempt to extract a matching Msg within a MsgQueue.
+tryChanMatch :: ChanMatch -> MsgQueue -> Maybe (MsgQueue, Msg)
+tryChanMatch (ChanMatch (i,mm)) q = do
+    ms <- lookup i (unMsgQueue q) -- Lookup corresponding messages
+    (ms',m) <- tryMsgMatch mm ms  -- take the first match
+    return (MsgQueue $ insert i ms' (unMsgQueue q), m)
+
+-- | Given a collection of ChanMatch's, extract a set of matching Msgs when
+-- each ChanMatch is successful.
+tryChanMatches :: ChanMatches -> MsgQueue -> Maybe (MsgQueue, Msgs)
+tryChanMatches []       q = Just (q,[])
+tryChanMatches (cm:cms) q = do
+    (q',m)   <- tryChanMatch cm q
+    (q'',ms) <- tryChanMatches cms q'
+    return (q'',m:ms)
+
+-- | Given a MatchRule and MessageQueues, test the rule, if successful
+-- returning the updated MessageQueues and the matching Messages.
+tryMatchRule :: MatchRule -> MsgQueue -> Maybe (MsgQueue, Msgs)
+tryMatchRule (MatchRule cms _) = tryChanMatches cms
+
+
+
+extractReplyIds :: ChanMatches -> Msgs -> [(ChanId,ChanId)]
+extractReplyIds = extractReplyIds' []
+  where
+    extractReplyIds' :: [(ChanId,ChanId)] -> ChanMatches -> Msgs -> [(ChanId,ChanId)]
+    extractReplyIds' acc []          []                     = acc
+    extractReplyIds' acc (_:cms)     (Msg (_,Nothing) : ms) = extractReplyIds' acc cms ms
+    extractReplyIds' acc (ChanMatch (i,_) : cms) (Msg (_,Just j) : ms)  = extractReplyIds' ((i,j):acc) cms ms
+
