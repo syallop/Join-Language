1a662a7db573bc011dbb9e78fd2eee2e3167fe40
Samuel A. Yallop <syallop@gmail.com>
2014-03-10 19:18:31 +0000

    Adds buffer example.

--- Join/Language/Examples.hs
+++ Join/Language/Examples.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE DataKinds #-}
 
 {-|
 Module      : Join.Language.Examples
@@ -14,8 +15,12 @@ module Join.Language.Examples where
 import Join.Language
 import Join.Types
 
+import Prelude hiding (take)
+
 import Control.Applicative
+import Control.Concurrent (threadDelay)
 import Control.Monad.IO.Class
+import Data.Serialize (Serialize)
 
 countDown :: Int -> ProcessM ()
 countDown n = do
@@ -45,4 +50,40 @@ fibonacci i = do
                                     j <- sync fib (n-2)
                                     reply fib (wait i + wait j))
     wait <$> sync fib i
+
+{- Buffer example: -}
+
+newtype Buffer a = Buffer (Chan a, SyncChan () a)
+mkBuffer :: Serialize a => ProcessM (Buffer a)
+mkBuffer = do
+    p <- newChannel           -- put channel  :: Chan a
+    t <- newChannel           -- take channel :: SyncChan a ()
+    (t & p) |> (\_-> reply t) -- reply put's to take's
+    return $ Buffer (p,t)
+
+-- | Asynchronously put a message on the buffer.
+put :: Serialize a => Buffer a -> a -> ProcessM ()
+put (Buffer (p,_)) = send p
+
+-- | Synchronously take a message on the buffer.
+take :: Serialize a => Buffer a -> ProcessM (SyncVal a)
+take (Buffer (_,t)) = sync t ()
+
+bufferExample :: ProcessM Int
+bufferExample = do
+    -- Create a new Buffer
+    b <- mkBuffer
+
+    -- Put values in
+    put b 1
+    put b 2
+
+    -- Do things, wait a bit
+    liftIO $ putStrLn "Waiting..." >> threadDelay 100000
+
+    -- Get values out and do something with them
+    i <- take b
+    j <- take b
+
+    return $ wait i + wait j
 
