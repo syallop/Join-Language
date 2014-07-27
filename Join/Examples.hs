922194278a0b91afdadf3b39539e12ef29c7b796
Samuel A. Yallop <syallop@gmail.com>
2014-07-27 03:26:04 +0100

    Extracts example concurrency objects from Example
    - Extracts example concurrency objects (lock,barrier,etc) from Example
      into their own module, using export mechanism to hide
      internals as required.

--- Join/Examples.hs
+++ Join/Examples.hs
@@ -4,19 +4,21 @@
 
 module Join.Examples where
 
+import Prelude hiding (take, read)
+
 import Join.Language
 import Join.Types
-{-import Join.Interpretation.Simple-}
 import Join.Interpretation.Complex
 
-import Prelude hiding (take, read)
+import Join.Examples.Counter
+import Join.Examples.Buffer
+import Join.Examples.Lock
+import Join.Examples.Barrier
 
 import Control.Applicative
 import Control.Concurrent (threadDelay)
 import Control.Monad (liftM,replicateM_,replicateM)
 import Control.Monad.IO.Class
-import Data.Serialize hiding (get,put)
-import System.IO
 
 -- | Countdown all values to 0.
 countDown :: Int -> Process ()
@@ -39,8 +41,6 @@ countDown n = do
     -- Become inert.
     inert
 
-
-
 -- | Parallel computation of the fibonacci function.
 fibonacci :: Int -> Process Int
 fibonacci i = do
@@ -52,31 +52,7 @@ fibonacci i = do
     sync' fib i
 
 
-
-
 {- 'Counter' example: -}
-newtype Counter = Counter (SyncChan () (), SyncChan () Int)
-mkCounter :: Process Counter
-mkCounter = do
-    count <- newChannel -- :: Chan Int
-    inc   <- newChannel -- :: SyncChan () ()
-    get   <- newChannel -- :: SyncChan () Int
-
-    inc & count |> \_ n -> reply inc () `with` send count (n+1)
-    get & count |> \_ n -> reply get n  `with` send count n
-
-    send count 1
-
-    return $ Counter (inc,get)
-
--- | Increment counter, waiting until complete.
-inc :: Counter -> Process ()
-inc (Counter (i,_)) = sync i () >> inert
-
--- | Get current value, waiting until complete.
-get :: Counter -> Process Int
-get (Counter (_,g)) = sync' g ()
-
 -- | Increment and query a counter with implicit mutex.
 counterExample :: Process ()
 counterExample = do
@@ -93,28 +69,6 @@ counterExample = do
     get c >>= liftIO . print
 
     inert
-
-
-
-
-{- Buffer example: -}
-newtype Buffer a = Buffer (Chan a, SyncChan () a)
-mkBuffer :: Serialize a => Process (Buffer a)
-mkBuffer = do
-    p <- newChannel       -- put channel  :: Chan a
-    t <- newChannel       -- take channel :: SyncChan a ()
-    t & p |> \_-> reply t -- reply put's to take's
-    return $ Buffer (p,t)
-
--- | Asynchronously put a message on the buffer.
-put :: Serialize a => Buffer a -> a -> Process ()
-put (Buffer (p,_)) = send p
-
--- | Synchronously take a message on the buffer.
-take :: Serialize a => Buffer a -> Process (SyncVal a)
-take (Buffer (_,t)) = syncSignal t
-
-
 
 -- | Store some items in a buffer, retrieve them later. Simulates state.
 -- pred> bufferExample == 3
@@ -136,67 +90,13 @@ bufferExample = do
 
     return $ read i + read j
 
-
-
-
-{- Primitive Lock example -}
-newtype Lock = Lock (SyncSignal (),SyncSignal ())
-mkLock :: Process Lock
-mkLock = do
-    free   <- newChannel -- Enforce mutex      :: Chan ()
-    lock   <- newChannel -- Request for locks  :: SyncChan () ()
-    unlock <- newChannel -- Request for unlock :: SyncChan () ()
-
-    -- Only when free, reply to a lock request.
-    free & lock |> \_ -> reply lock
-
-    -- When unlock request, set free.
-    unlock      |> \_ -> signal free `with` reply unlock()
-
-    signal free
-    return $ Lock (lock,unlock)
-
--- | Block until a lock is acquired.
-lock :: Lock -> Process ()
-lock (Lock (l,_)) = sync' l ()
-
--- | Release a lock.
-unlock :: Lock -> Process ()
-unlock (Lock (_,u)) = sync' u ()
-
--- | Acquire lock before running a process. (unlocking afterward).
-withLock :: Lock -> Process () -> Process ()
-withLock l p = lock l >> p >> unlock l
-
 -- | Only one subprocess may hold the lock at a time.
 -- =>"One" "Two" or "Two" "One". No intermingling.
 lockExample :: Process ()
 lockExample = mkLock >>= \l -> withLock l (liftIO $ putStrLn "One")
                         `with` withLock l (liftIO $ putStrLn "two")
 
-
-
-
-
 {- Barrier example -}
-newtype Barrier = Barrier (SyncChan () (), SyncChan () ())
-mkBarrier :: Process Barrier
-mkBarrier = do
-    l <- newChannel
-    r <- newChannel
-    l & r |> \_ _ -> reply l () `with` reply r ()
-    return $ Barrier (l,r)
-
--- | 'Left side' waits at barrier.
-signalLeft :: Barrier -> Process ()
-signalLeft (Barrier (l,_)) = syncSignal' l
-
--- | 'Right side' waits at barrier.
-signalRight :: Barrier -> Process ()
-signalRight (Barrier (_,r)) = syncSignal' r
-
--- | Barriers enforce a subProcesses move in step.
--- => Result is "(lr)" or "(rl)".
 barrierExample :: Process ()
 barrierExample = do
     b <- mkBarrier
