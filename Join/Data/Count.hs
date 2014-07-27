8f48e7242101480196be0621d10a3a56ca5d36f5
Samuel A. Yallop <syallop@gmail.com>
2014-07-27 05:47:19 +0100

    Adds Count Data

---
+++ Join/Data/Count.hs
@@ -0,0 +1,28 @@
+module Join.Data.Count
+    ( mkCount
+    , waitZero
+    , tick
+    ) where
+
+import Join
+
+newtype Count = Count (SyncSignal (), Signal)
+
+mkCount :: Int -> Process Count
+mkCount i = do
+    count <- newChannel
+    tick  <- newChannel
+    zero  <- newChannel
+
+    count    & tick |> \n () -> reply count (n-1)
+    count&=0 & zero |> \()   -> reply zero ()
+
+    sync count i
+    return $ Count (zero,tick)
+
+waitZero :: Count -> Process ()
+waitZero (Count (z,_)) = syncSignal' z
+
+tick :: Count -> Process ()
+tick (Count (_,t)) = signal t
+
