2eb57946a871356b550692e1d14a636c42498b7e
Samuel A. Yallop <syallop@gmail.com>
2014-11-16 19:02:59 +0000

    Removes intermediate data-structure of patterns
    - Reimplements '&','&=','&~','|>' and '|$' directly as regular functions
      instead of as data constructors and instances.

--- Join/Pattern/Clause.hs
+++ Join/Pattern/Clause.hs
@@ -1,37 +1,13 @@
-{-# LANGUAGE DataKinds
-            ,GADTs
-            ,FlexibleInstances
-            ,TypeOperators
-            ,MultiParamTypeClasses
-  #-}
 module Join.Pattern.Clause
-  (DefinitionClause(..)
-  ,(|>)
+  ((|>)
   ) where
 
 import Join.Apply
 import Join.Pattern.Rep
 
--- | A DefinitionClause is a pattern type and an associated trigger function.
---
--- Declared infix via '|>'.
-data DefinitionClause ts tr r where
-  DefinitionClause :: (ToPatterns pat ts,HasTriggerType ts tr r,Apply tr r)
-                   => pat
-                   -> tr
-                   -> DefinitionClause ts tr r
-
--- | Infix 'DefinitionClause'.
+-- | Build a definition infix from a patterns type and an associated trigger function.
 (|>) :: (ToPatterns pat ts,HasTriggerType ts tr r,Apply tr r)
-     => pat
-     -> tr
-     -> DefinitionClause ts tr r
+     => pat -> tr -> Definition ts tr r
 infixr 6 |>
-(|>) = DefinitionClause
-
-instance ToDefinition (DefinitionClause ts tr r) ts tr r
-  where toDefinition (DefinitionClause pat tr) = Definition (toPatterns pat) (Trigger tr)
-
-instance ToDefinitions (DefinitionClause ts tr r) '[Definition ts tr r] r
-  where toDefinitions (DefinitionClause pat tr) = OneDefinition (Definition (toPatterns pat) (Trigger tr))
+ps |> tr = Definition (toPatterns ps) (Trigger tr)
 
--- Join/Pattern/Pred.hs
+++ Join/Pattern/Pred.hs
@@ -1,11 +1,5 @@
-{-# LANGUAGE DataKinds
-            ,FlexibleInstances
-            ,GADTs
-            ,MultiParamTypeClasses
-  #-}
 module Join.Pattern.Pred
-  ( ChannelPred(..)
-  , (&~)
+  ( (&~)
   ) where
 
 import Join.Pattern.Rep
@@ -18,21 +12,10 @@ import Data.Typeable
 -- | Pattern type of matching messages sent on a 'Channel' ONLY when they satisfy
 -- some predicate.
 --
--- Declared infix via '&~'.
---
 -- E.G. @ intChan&~(<10) @
 -- Then a trigger is typed: @ trigger :: Int -> return @
 -- and may only fire when the sent message is less than 10.
-data ChannelPred s a = MessageType a => ChannelPred (Channel s a) (a -> Bool)
-
--- | Infix 'ChannelPred'
-(&~) :: MessageType a => Channel s a -> (a -> Bool) -> ChannelPred s a
+(&~) :: (MessageType a,Typeable s) => Channel s a -> (a -> Bool) -> Pattern s a Pass
 infixr 8 &~
-(&~) = ChannelPred
-
-instance Typeable s => ToPattern (ChannelPred s a) s a Pass
-  where toPattern (ChannelPred c p) = Pattern c (MatchWhen p) DoPass
-
-instance Typeable s => ToPatterns (ChannelPred s a) '[Pattern s a Pass]
-  where toPatterns (ChannelPred c p) = OnePattern $ Pattern c (MatchWhen p) DoPass
+c &~ pred = Pattern c (MatchWhen pred) DoPass
 
