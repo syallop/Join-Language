8b8acf0e145409fd3f05f8fe21827834d8b0d6f3
Samuel A. Yallop <syallop@gmail.com>
2015-01-19 01:14:54 +0000

    Adds a small DSL for distributed programming
    - Adds Language/Distributed giving a small DSL for looking up and
      registering Channels.

---
+++ Join/Language/Distributed.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE ConstraintKinds
+            ,DataKinds
+            ,FlexibleContexts
+            ,FlexibleInstances
+            ,GADTs
+            ,KindSignatures
+            ,MultiParamTypeClasses
+            ,RankNTypes
+            ,TemplateHaskell
+            ,TypeOperators
+            ,TypeSynonymInstances
+ #-}
+
+module Join.Language.Distributed
+  ( Dist
+  , DistInst(..)
+
+  , lookupChannel
+  , registerChannel
+  ) where
+
+import Join.Channel
+
+import DSL.Program
+
+type Name = String
+
+data DistInst (p :: * -> *) (a :: *) where
+
+  LookupChannel
+      :: MessageType a
+      => Name
+      -> DistInst p (Maybe (Channel A a))
+
+  RegisterChannel
+      :: MessageType a
+      => Name
+      -> Channel A a
+      -> DistInst p Bool
+
+type Dist a = Program DistInst a
+
+type UsingDist a = ProgramUsing DistInst a
+
+lookupChannel :: MessageType a => Name -> UsingDist (Maybe (Channel A a))
+lookupChannel n = inject $ LookupChannel n
+
+registerChannel :: MessageType a => Name -> Channel A a -> UsingDist Bool
+registerChannel n c = inject $ RegisterChannel n c
+
